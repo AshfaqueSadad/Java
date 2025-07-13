34c83a9bb5cda599904e86a748458027d717d772
AshfaqueSadad <ashfaquesadad7@gmail.com>
2025-07-13 12:28:51 +0600

    Add files via upload

---
+++ Polymorphism.java
@@ -0,0 +1,13 @@
+public class Polymorphism {
+
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    double add(double a, double b) {
+        return a + b;
+    }
+
+    // that is compile time polymorphism , also known as method overloading
+
+}
